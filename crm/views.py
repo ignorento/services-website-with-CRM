a1f2b4e95a5f790ff27df6e9a9bcb1b3c878ceb2
ignorento <babin.architect@gmail.com>
2023-10-11 02:52:48 +0300

    connect slider support

--- crm/views.py
+++ crm/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
-
+from slider.models import SliderContent
 
 def first_page(request):
-    object_list = Order.objects.all()
-    form = OrderForm()
-    return render(request, './index.html', {'object_list': object_list,
-                                            'form': form})
+    # object_list = Order.objects.all()
+    # form = OrderForm()
+    slider_list = SliderContent.objects.all()
+
+    return render(request, './index.html', {'slider_list': slider_list
+                                            })
 
 
 def thanks_page(request):
