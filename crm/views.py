0decfbe7f5d1d485f8cc196597190d94d3aba92b
ignorento <babin.architect@gmail.com>
2023-10-12 23:08:22 +0300

    update with price app

--- crm/views.py
+++ crm/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
 from slider.models import SliderContent
+from price.models import PriceTable, PriceCard
 
 def first_page(request):
     # object_list = Order.objects.all()
     # form = OrderForm()
     slider_list = SliderContent.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    dict_obj = {'slider_list': slider_list,
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'price_table': price_table,
+                }
 
-    return render(request, './index.html', {'slider_list': slider_list
-                                            })
+
+    return render(request, './index.html', dict_obj)
 
 
 def thanks_page(request):
