cb3e9d2289d69be50021e8d1227ede6f935fbdf5
ignorento <babin.architect@gmail.com>
2023-10-14 23:24:36 +0300

    update syntax

--- crm/admin.py
+++ crm/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from crm.models import Order, StatusCrm, CommentCrm
 
-# Register your models here.
 admin.site.register(Order)
 admin.site.register(StatusCrm)
 admin.site.register(CommentCrm)
