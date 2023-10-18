492e001f9f10dd54b177bf61be07a52610d36e11
ignorento <babin.architect@gmail.com>
2023-10-19 02:04:57 +0300

    update admin panel in slider

--- slider/admin.py
+++ slider/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import SliderContent
 
-admin.site.register(SliderContent)
+
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ('slider_title', 'slider_text', 'slider_css', 'get_img')
+    list_display_links = ('slider_title',)
+    list_editable = ('slider_css',)
+
+    fields = ('slider_title', 'slider_text', 'slider_css', 'slider_img', 'get_img')
+    readonly_fields = ('get_img',)
+
+    def get_img(self, obj):
+        if obj.slider_img:
+            return mark_safe(f'<img src="{obj.slider_img.url}" width="80px"')
+        else:
+            return 'image NOT found'
+
+    get_img.short_description = 'Attachment'
+
+
+admin.site.register(SliderContent, SliderAdmin)
